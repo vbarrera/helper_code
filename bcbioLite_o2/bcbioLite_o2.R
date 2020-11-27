14d88e99de944d50fac4e8c4f108c4f3aceb35fc
Victor Barrera <Barrera.Vic@gmail.com>
2020-11-27 17:03:03 +0100

    bcbioLite_o2 initial commit

---
+++ bcbioLite_o2/bcbioLite_o2.R
@@ -0,0 +1,29 @@
+
+# Check if bcbioLite is installed. If not, install it with all upgrades.
+if("remotes" %in% rownames(installed.packages())){
+  library(bcbioLite)
+} else{
+  install.packages("remotes")
+  library(bcbioLite)
+}
+
+if("bcbioLite" %in% rownames(installed.packages())){
+  library(bcbioLite)
+} else{
+  remotes::install_github("vbarrera/bcbioLite", upgrade = "always")
+  library(bcbioLite)
+}
+
+project_dir <- commandArgs(trailingOnly=T)[1]
+output_dir <- commandArgs(trailingOnly=T)[1]
+output_dir <- normalizePath(output_dir)
+
+invisible(mapply(
+  FUN = dir.create,
+  list(output_dir),
+  MoreArgs = list(recursive = TRUE, showWarnings = FALSE)
+))
+
+se <- bcbioLite::bcbreader(project_dir)
+
+saveRDS(se, file = file.path(output_dir, paste0("se-",Sys.Date(),".rds")))
