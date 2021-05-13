21de644fc27d44818c5482a5cdf067bc8e9c633a
Victor Barrera <Barrera.Vic@gmail.com>
2021-05-13 13:06:16 +0200

    process cellranger ouput file and print found barcodes

--- find_barcode/find_barcode.py
+++ find_barcode/find_barcode.py
@@ -6,36 +6,38 @@ Created on Wed May 12 13:56:43 2021
 @author: victorbarrera
 """
 
-import sys
-import itertools
+from sys import argv
+from itertools import permutations, dropwhile
 
 
-
-
-barcodes_file=sys.argv[1]
-unknown_barcodes_file=sys.argv[2]
-
-barcodes_file_fh=open(barcodes_file,'r')
-unknown_barcodes_file_fh=open(unknown_barcodes_file,'r')
+barcodes_file=argv[1]
+unknown_barcodes_file=argv[2]
 
 
 barcodes_dict = {}
 
-next(barcodes_file_fh)
-for barcode_group in barcodes_file_fh:
-    barcode_group_id=str(barcode_group.rstrip().split(",")[0])
-    index_i7=str(barcode_group.rstrip().split(",")[1])
-    index_ai5=str(barcode_group.rstrip().split(",")[2])
-    index_bi5=str(barcode_group.rstrip().split(",")[3])
-    comb_list=list(itertools.permutations([index_i7,index_ai5,index_bi5], 2))
-    for comb in comb_list:
-        barcodes_dict[str("+".join(comb))]=barcode_group_id
+with open(barcodes_file,'r') as barcodes_file_fh:
+    next(barcodes_file_fh)
+    for barcode_group in barcodes_file_fh:
+        barcode_group_id=str(barcode_group.rstrip().split(",")[0])
+        index_i7=str(barcode_group.rstrip().split(",")[1])
+        index_ai5=str(barcode_group.rstrip().split(",")[2])
+        index_bi5=str(barcode_group.rstrip().split(",")[3])
+        comb_list=list(permutations([index_i7,index_ai5,index_bi5], 2))
+        for comb in comb_list:
+            barcodes_dict[str("+".join(comb))]=barcode_group_id
 
-for key_val in barcodes_dict.keys():
-    print('%s\t' %
-          (key_val,
-           ))
+def barcode_section(line): return line != '### Columns: Index_Sequence Hit_Count\n'
+barcodes_found=[]
     
-
-barcodes_file_fh.close()
-unknown_barcodes_file_fh.close()
+with open(unknown_barcodes_file,'r') as unknown_barcodes_file_fh:
+    for line in dropwhile(barcode_section, unknown_barcodes_file_fh):
+        if line.startswith("#"):
+            next
+        else:
+            barcode_unknown_found=str(line.rstrip().split()[0])
+            if barcode_unknown_found in barcodes_dict.keys():
+                barcodes_found.append(barcodes_dict[barcode_unknown_found])
+                
+print("Barcodes found:")
+print(barcodes_found)
